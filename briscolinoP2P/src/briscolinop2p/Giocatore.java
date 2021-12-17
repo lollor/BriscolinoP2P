ce813e90985afedf0b154cc96796df548113472b
GerosaSimone <s.gerosa0@gmail.com>
2021-12-17 09:57:54 +0100

    creazione tavolo,giocatore,carte
    crezione classi base

---
+++ briscolinoP2P/src/briscolinop2p/Giocatore.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package briscolinop2p;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gerosa_simone
+ */
+public class Giocatore {
+    String nome;
+    String punteggio;
+    ArrayList<Carta> mano;//tre carte che ha in mano il giocatore
+    
+}
---
+++ briscolinoP2P/src/briscolinop2p/Tavolo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package briscolinop2p;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gerosa_simone
+ */
+public class Tavolo {
+    ArrayList<Carta> mazzo;
+    Giocatore mazziere;
+    Giocatore Sfidante;
+    
+}
