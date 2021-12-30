ea8aeb7acefd349f96909078614a9780c3e852cf
GerosaSimone <s.gerosa0@gmail.com>
2021-12-30 12:36:10 +0100

    aggiornamento version
    aggiornamento versione di giocatore,carta,tavolo

---
+++ briscolinoP2P/src/briscolinop2p/Tavolo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package briscolinop2p;
+
+import briscolinop2p.Carta;
+import briscolinop2p.Giocatore;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author gerosa_simone
+ */
+public class Tavolo {
+    ArrayList<Carta> mazzo;
+    Giocatore mazziere;
+    Giocatore Sfidante;
+    
+}
